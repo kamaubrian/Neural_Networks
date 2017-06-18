c6086d3526d849981aeeec30323e0fc81b769dc9
mtotodev05 <mtotodev05@gmail.com>
2017-06-18 14:16:38 +0300

    Funct

--- neural.cpp
+++ neural.cpp
@@ -2,10 +2,15 @@
 // Created by brian-kamau on 6/18/17.
 //
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Net{
 public:
+    Net(topology);
+    void feedForward(const vector<double> &inputval);
+    void backProp(const vector<double> &targetval);
+    void getResults( vector<double> &resultval) const;
 
 
 private:
@@ -15,7 +20,16 @@ private:
 };
 int main(){
     cout<<"Welcome to Neural Networks"<<endl;
+
+    vector<double> inputvals;
+    vector<double> targetvals;
+    vector <double> resultvals;
+
     Net myNet(topology);
+
+    myNet.feedForward(inputvals);
+    myNet.backProp(targetvals);
+    myNet.getResults(resultvals);
 }
 
 
