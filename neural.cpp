1e31afc031dadfbb0846ffbf3b9b1cc540e8b39c
mtotodev05 <mtotodev05@gmail.com>
2017-06-19 16:02:15 +0300

    Neuron

--- neural.cpp
+++ neural.cpp
@@ -3,13 +3,33 @@
 //
 #include <iostream>
 #include <vector>
+#include <cstdlib>
 
 using namespace std;
-class Neuron{
-
+struct Connection{
+    double weight;
+    double deltaweight;
 
 
 };
+class Neuron{
+public:
+    Neuron(unsigned numoutput);
+private:
+    static double randomWeight(void){ return rand()/double (RAND_MAX);}
+    double m_outputVal;
+    vector<Connection> m_outputweights;
+
+
+};
+Neuron::Neuron(unsigned numoutput) {
+    for(unsigned c=0;c<numoutput;c++){
+        m_outputweights.push_back(Connection());
+        m_outputweights.back().weight=randomWeight();
+
+    }
+
+}
 typedef vector<Neuron> Layer;
 
 class Net{
@@ -37,16 +57,14 @@ Net::Net(vector<unsigned> &topology) {
     for(unsigned layerNum = 0;layerNum<numLayers;layerNum++){
 
         m_layers.push_back(Layer());
+        unsigned numofOutputs = layerNum == topology.size() -1?0:topology[layerNum+1];
 
         for(unsigned neuroNum =0;neuroNum<=topology[layerNum];neuroNum++){
-            m_layers.back().push_back(Neuron());
-            cout<<"Made a Neuron"<<endl;
+            m_layers.back().push_back(Neuron(numofOutputs));
+            cout<<"\tMade a Neuron"<<endl;
         }
 
     }
-
-
-
 }
 
 using namespace std;
